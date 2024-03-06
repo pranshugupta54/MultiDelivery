fcefccacc76f98e9a0727f53e7c39ecc3e52d378
Pranshu <pranshgupta54@gmail.com>
2024-03-06 15:47:42 +0530

    tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -8,9 +8,9 @@ function App() {
 
   return (
     <>
-      <div>
+      <div className="bg-red-200 ">
         <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
+          <img src={viteLogo} />
         </a>
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
