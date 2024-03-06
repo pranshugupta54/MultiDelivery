eda63358075a2a3edb5a7842014a4b4c1bfb2caa
Saurav Gupta <er.sauravgpt@gmail.com>
2024-03-06 19:07:21 +0530

    folder structure

---
+++ client/src/app/(root)/trackOrder/[id]/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function trackOrder() {
+  return (
+    <div>
+      Track Order
+    </div>
+  )
+}
+
+export default trackOrder
---
+++ client/src/app/(root)/users/customer/[id]/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function CustomerDashboard() {
+  return (
+    <div>
+      customerDashboard
+    </div>
+  )
+}
+
+export default CustomerDashboard
